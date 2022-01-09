9071d706c706205ecb2aa20b487df6d0b2278596
NinekoTheCat <cnotsomark@gmail.com>
2022-01-09 16:06:27 +0100

    simplified Catconomy.kt and simplified CatBalanceHandler.kt

--- src/main/kotlin/ninekothecat/catconomy/defaultImplementations/CatBalanceHandler.kt
+++ src/main/kotlin/ninekothecat/catconomy/defaultImplementations/CatBalanceHandler.kt
@@ -1,23 +1,21 @@
 package ninekothecat.catconomy.defaultImplementations
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import ninekothecat.catconomy.Catconomy
 import ninekothecat.catplugincore.money.enums.TransactionResult
 import ninekothecat.catplugincore.money.enums.TransactionType
 import ninekothecat.catplugincore.money.interfaces.IBalanceHandler
 import ninekothecat.catplugincore.money.interfaces.ITransaction
 import java.util.*
-import java.util.concurrent.Executors
-import java.util.concurrent.ThreadPoolExecutor
 
 class CatBalanceHandler(private val doLogs: Boolean) : IBalanceHandler {
-    private val threadPoolExecutor: ThreadPoolExecutor = Executors.newCachedThreadPool() as ThreadPoolExecutor
     override fun doTransaction(transaction: ITransaction): TransactionResult {
         return try {
             if (Catconomy.permissionGuard!!.isPermitted(transaction)) {
-                val resultFuture = threadPoolExecutor.submit<TransactionResult> { getTransactionResult(transaction) }
-                val result = resultFuture.get()
-                logTransaction(transaction, result)
-                result
+                val resultFuture = runBlocking(Dispatchers.IO) { getTransactionResult(transaction) }
+                logTransaction(transaction, resultFuture)
+                resultFuture
             } else {
                 Catconomy.iCatLogger!!.fail(transaction, TransactionResult.LACK_OF_PERMS)
                 TransactionResult.LACK_OF_PERMS
