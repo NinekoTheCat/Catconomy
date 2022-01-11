4d2081a90784912dbddbf784356aa4aad0b99cd9
NinekoTheCat <cnotsomark@gmail.com>
2022-01-12 00:41:38 +0100

    evicted MessageFormat and String.Format from CatLogger

--- src/main/kotlin/ninekothecat/catconomy/logging/CatLogger.kt
+++ src/main/kotlin/ninekothecat/catconomy/logging/CatLogger.kt
@@ -8,51 +8,38 @@ import org.apache.logging.log4j.Logger
 import org.apache.logging.log4j.Marker
 import org.apache.logging.log4j.MarkerManager.Log4jMarker
 import org.apache.logging.log4j.core.LoggerContext
-import java.text.MessageFormat
+import org.bukkit.plugin.Plugin
 import java.util.*
 
 class CatLogger : ICatLogger {
     private var logger: Logger? = null
     override fun success(transaction: ITransaction) {
-        val marker: Marker = Log4jMarker(
-            "${transaction.plugin.name} V${transaction.plugin.description.version}"
-        )
+        val marker: Marker = generateLog4J2Marker(transaction.plugin)
         logger!!.info(marker, transaction.message)
     }
 
     override fun fail(transaction: ITransaction, result: TransactionResult) {
-        val marker: Marker = Log4jMarker(
-            "${transaction.plugin.name} V${transaction.plugin.description.version}"
-        )
+        val marker: Marker = generateLog4J2Marker(transaction.plugin)
         logger!!.warn(
             marker,
-            MessageFormat.format(
-                "Transaction with type {0} Failed because {1} with amount {2} initiated by {3} with users {4}",
-                transaction.transactionType.toString(), result.toString(), transaction.amount, transaction.initiator,
-                transaction.usersInvolved!!.toTypedArray().contentToString()
-            )
+            "Transaction with type ${transaction.transactionType} Failed because $result with " +
+                    "amount ${transaction.amount} initiated by ${transaction.initiator} " +
+                    "with users ${transaction.usersInvolved!!.toTypedArray().contentToString()}"
         )
     }
 
     override fun error(transaction: ITransaction, result: TransactionResult, exception: Exception?) {
-        val marker: Marker = Log4jMarker(
-            String.format(
-                "%s V%s", transaction.plugin.name,
-                transaction.plugin.description.version
-            )
-        )
+        val marker: Marker = generateLog4J2Marker(transaction.plugin)
         logger!!.error(
             marker,
-            MessageFormat.format(
-                "Transaction with type {0} failed because {1} with amount {2} initiated by {3} with users {4}",
-                transaction.transactionType.toString(),
-                result.toString(),
-                transaction.amount,
-                transaction.initiator,
-                transaction.usersInvolved!!.toTypedArray().contentToString()
-            ), exception
+            "Transaction with type ${transaction.transactionType} failed because $result with " +
+                    "amount ${transaction.amount} initiated by ${transaction.initiator} " +
+                    "with users ${transaction.usersInvolved!!.toTypedArray().contentToString()}", exception
         )
     }
+
+    private fun generateLog4J2Marker(plugin: Plugin): Log4jMarker =
+        Log4jMarker("${plugin.name} V${plugin.description.version}")
 
     init {
         try {
